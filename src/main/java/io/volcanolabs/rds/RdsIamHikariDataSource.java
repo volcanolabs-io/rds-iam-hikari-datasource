53bd923fbbf0d87769d822c2f683fd2f91ddf53e
John McPeek <john@volcanolabs.io>
2022-09-03 09:06:37 -0400

    Removed SSM and moved to using an override environment variable.

--- src/main/java/io/volcanolabs/rds/RdsIamHikariDataSource.java
+++ src/main/java/io/volcanolabs/rds/RdsIamHikariDataSource.java
@@ -4,15 +4,10 @@ import com.zaxxer.hikari.HikariDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
-import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.regions.providers.DefaultAwsRegionProviderChain;
 import software.amazon.awssdk.services.rds.RdsUtilities;
 import software.amazon.awssdk.services.rds.model.GenerateAuthenticationTokenRequest;
-import software.amazon.awssdk.services.ssm.SsmClient;
-import software.amazon.awssdk.services.ssm.SsmClientBuilder;
-import software.amazon.awssdk.services.ssm.model.GetParameterRequest;
-import software.amazon.awssdk.services.ssm.model.GetParameterResponse;
 
 import java.net.URI;
 
@@ -22,7 +17,7 @@ import java.net.URI;
  */
 public class RdsIamHikariDataSource extends HikariDataSource {
 	private static final Logger log = LoggerFactory.getLogger( RdsIamHikariDataSource.class );
-	public static final String PARAMETER_STORE_REGION_KEY = "PARAMETER_STORE_REGION_KEY";
+	public static final String RDS_REGION_OVERRIDE = "RDS_REGION_OVERRIDE";
 
 	public RdsIamHikariDataSource() {
 		log.trace( "RdsIamHikariDataSource created" );
@@ -60,23 +55,11 @@ public class RdsIamHikariDataSource extends HikariDataSource {
 		var region = new DefaultAwsRegionProviderChain().getRegion();
 		log.trace( "AWS region: {}", region );
 
-		String regionOverrideKey = System.getenv().get( PARAMETER_STORE_REGION_KEY );
-		if ( regionOverrideKey != null ) {
-			SsmClientBuilder ssmClientBuilder = SsmClient.builder()
-					.region( region )
-					.credentialsProvider( ProfileCredentialsProvider.create() );
+		String rdsRegionOverride = System.getenv().get( RDS_REGION_OVERRIDE );
+		if ( rdsRegionOverride != null ) {
+			log.info( "RDS region override: {}", rdsRegionOverride );
 
-			try ( SsmClient ssmClient = ssmClientBuilder.build() ) {
-				GetParameterRequest parameterRequest = GetParameterRequest.builder()
-						.name( regionOverrideKey )
-						.build();
-				GetParameterResponse parameterResponse = ssmClient.getParameter( parameterRequest );
-				var paramValue = parameterResponse.parameter().value();
-				var overrideRegion = Region.of( paramValue );
-				log.trace( "AWS region: {}", overrideRegion );
-
-				return overrideRegion;
-			}
+			return Region.of( rdsRegionOverride  );
 		}
 		else {
 			return region;
