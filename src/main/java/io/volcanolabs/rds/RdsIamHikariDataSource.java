bdceb6c7aca8d6c595ca5483c929bf9ecdcac464
volcanolabs-io <96843561+volcanolabs-io@users.noreply.github.com>
2022-09-12 19:00:04 -0400

    V2sdk (#4)
    * switch to aws sdk v2 and added region override
    
    * added setting the port back in
    
    * Removed SSM and moved to using an override environment variable.
    
    * Removed SSM and moved to using an override environment variable.
    
    * Removed SSM and moved to using an override environment variable.
    
    * Time for a new version. 2.0.0 to reflect the change to AWS SDK version 2.

--- src/main/java/io/volcanolabs/rds/RdsIamHikariDataSource.java
+++ src/main/java/io/volcanolabs/rds/RdsIamHikariDataSource.java
@@ -1,12 +1,13 @@
 package io.volcanolabs.rds;
 
-import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
-import com.amazonaws.regions.DefaultAwsRegionProviderChain;
-import com.amazonaws.services.rds.auth.GetIamAuthTokenRequest;
-import com.amazonaws.services.rds.auth.RdsIamAuthTokenGenerator;
 import com.zaxxer.hikari.HikariDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.regions.providers.DefaultAwsRegionProviderChain;
+import software.amazon.awssdk.services.rds.RdsUtilities;
+import software.amazon.awssdk.services.rds.model.GenerateAuthenticationTokenRequest;
 
 import java.net.URI;
 
@@ -16,6 +17,7 @@ import java.net.URI;
  */
 public class RdsIamHikariDataSource extends HikariDataSource {
 	private static final Logger log = LoggerFactory.getLogger( RdsIamHikariDataSource.class );
+	public static final String RDS_REGION_OVERRIDE = "RDS_REGION_OVERRIDE";
 
 	public RdsIamHikariDataSource() {
 		log.trace( "RdsIamHikariDataSource created" );
@@ -28,25 +30,39 @@ public class RdsIamHikariDataSource extends HikariDataSource {
 	}
 
 	private String getToken() {
-		var region = new DefaultAwsRegionProviderChain().getRegion();
-		log.trace( "AWS region: {}", region );
-
-		RdsIamAuthTokenGenerator generator = RdsIamAuthTokenGenerator.builder()
-				.credentials( new DefaultAWSCredentialsProviderChain() )
-				.region( region )
-				.build();
+		var region = getRegion();
 
 		// JDBC URL has a standard URL format, like: jdbc:postgresql://localhost:5432/test_database
 		var cleanUrl = getJdbcUrl().substring( 5 );
 		log.trace( "cleanUrl: {}", cleanUrl );
 		var dbUri = URI.create( cleanUrl );
 
-		GetIamAuthTokenRequest request = GetIamAuthTokenRequest.builder()
+		GenerateAuthenticationTokenRequest authTokenRequest = GenerateAuthenticationTokenRequest.builder()
 				.hostname( dbUri.getHost() )
 				.port( dbUri.getPort() )
-				.userName( getUsername() )
+				.username( getUsername() )
 				.build();
 
-		return generator.getAuthToken( request );
+		RdsUtilities utilities = RdsUtilities.builder()
+				.credentialsProvider( DefaultCredentialsProvider.create() )
+				.region( region )
+				.build();
+
+		return utilities.generateAuthenticationToken( authTokenRequest );
+	}
+
+	private Region getRegion() {
+		var region = new DefaultAwsRegionProviderChain().getRegion();
+		log.trace( "AWS region: {}", region );
+
+		String rdsRegionOverride = System.getenv().get( RDS_REGION_OVERRIDE );
+		if ( rdsRegionOverride != null ) {
+			log.info( "RDS region override: {}", rdsRegionOverride );
+
+			return Region.of( rdsRegionOverride );
+		}
+		else {
+			return region;
+		}
 	}
 }
